5099df910b5be869cf94bf58210ed49d41864e5c
Bekosantux <Bekosantux@users.noreply.github.com>
2025-09-01 09:36:15 +0000

    deploy: bca367dc033ce97f97b4ad4accff7ef88eedfc17

---
+++ preview/assets/js/aba21aa0.d31725ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkshop_document=self.webpackChunkshop_document||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
