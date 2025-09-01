b72efbf32f062e2d3f936847f5418602163e1817
Bekosan <tankabeko@gmail.com>
2025-09-02 03:58:34 +0900

    Changed:  custom subdomain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://beko.ooo/',
+  url: 'https://shop.beko.ooo/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/shop-document/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
